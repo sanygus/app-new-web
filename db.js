92ff5fae5b343ca8d6dda508eeecd9d105fd5f06
san <san@sansystem>
2017-10-06 00:27:00 +0300

    sensors last fix

--- db.js
+++ db.js
@@ -9,5 +9,5 @@ MongoClient.connect('mongodb://localhost:27017/exapp', function(err, dblink) {
 });
 
 module.exports.getSensors = (devid, callback) => {
-  db.collection('sensors').find({ devid }, { _id: 0, devid: false }).sort({ "date": 1 }).limit(100).toArray(callback);
+  db.collection('sensors').find({ devid }, { _id: 0, devid: false }).sort({ "date": -1 }).limit(100).toArray((err, data) => { callback(err, data.reverse()); });
 }
